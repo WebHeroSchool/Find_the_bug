a70b650f7b3ae8c89e7c689d7021982a85d5df9f
Elvira Snezhkova <eli4kina@gmail.com>
2021-07-16 15:57:40 +0700

    Адаптация под разные экраны

--- script.js
+++ script.js
@@ -7,16 +7,12 @@ function setCards (level) {
 
   const card = document.getElementById('card');
   console.log(card);
-  let innerCard = card.childNodes[1];
   
   for (let i=1; i<level; i++) {
     let cardN = card.cloneNode(true); // клонирование карты n раз
     cardN.id = card.id + i;
-    cardN.childNodes[1].id = innerCard.id + i;
-    console.log(cardN.childNodes[1]);
     card.after(cardN);
   }
-  innerCard.id =  innerCard.id + level; 
 
   //одну из карт сделаем багом
   let numBug = getRandomInt(1, level);
@@ -27,33 +23,38 @@ function setCards (level) {
 
 //функция по созданию события поворота на каждую карту
 function flipCard(level) {
-
-  for (let i=1; i<=level; i++) {
-    const idElem = 'inner' + i;
-    const flip = document.getElementById(idElem);
-    flip.addEventListener('click', event => flip.classList.add('rotation'));
+  cards = document.getElementsByClassName('card');
+  console.log(cards);
+  for (let i=0; i<cards.length; i++) {
+    cards[i].addEventListener('click', rotationCart);
   }
 }
 
-function setRestartForCards(level) {
-  for (let i=1; i<=level; i++) {
+function rotationCart() {
+  console.log("функция переворота");
+  this.classList.add('rotation');
+  
+  Array.from(cards).forEach(card => card.removeEventListener('click', rotationCart));
+  this.classList.remove('rotation');
+  Array.from(cards).forEach(card => card.addEventListener('click', restartCartTable));
+
+  /*for (let i=1; i<=level; i++) {
     const idElem = 'inner' + i;
-    const flip = document.getElementById(idElem);
-    card.addEventListener('click', restartCartTable(level));
-  }
+    const cart = document.getElementById(idElem);
+    cart.addEventListener('click',  restartCartTable(level));
+  }*/
 }
 
-function restartCartTable(level) {
+function restartCartTable() {
   console.log("сюда зашли");
   //главное окно сделали активным, карточный стол скрыли
   content.classList.remove('deactivate');
   cardTable.classList.add('deactivate');
 
   //удалили все карты
-  for (let i=1; i<level; i++) {
-    const idElem = 'card' + i;
-    const card = document.getElementById(idElem);
-    card.remove();
+  cards = document.getElementsByClassName('card');
+  for (let i=1; i<cards.length; i++) {
+    cards[i].remove();
   }
 }
 
@@ -61,16 +62,22 @@ function restartCartTable(level) {
 function openCardTable() {
  
   let level = document.querySelector('input[name="level"]:checked').value;
+  if (level.length === 0) return;
+
   content.classList.add('deactivate');
   cardTable.classList.remove('deactivate');
   setCards(level);
+
+  /*cards = document.querySelectorAll('.card');
+  console.log(cards);
+  Array.from(cards).forEach(card => card.addEventListener('click', rotationCart()));
+  console.log(cards);*/
+
   flipCard(level);
-  setRestartForCards(level);
 }
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
-
 
   
