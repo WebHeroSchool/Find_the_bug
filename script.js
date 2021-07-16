199bb34cb694c8169ffb6edd3dde44e457577c15
Elvira Snezhkova <eli4kina@gmail.com>
2021-07-16 17:33:33 +0700

    Доработана логика

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ let cardTable = document.getElementById('card-table');
 //Установим количество карт на столе согластно уровню сложности
 function setCards (level) {
 
-  const card = document.getElementById('card');
+  const card = document.querySelector('.card');
   console.log(card);
   
   for (let i=1; i<level; i++) {
     let cardN = card.cloneNode(true); // клонирование карты n раз
-    cardN.id = card.id + i;
+    cardN.classList.add("copy");
     card.after(cardN);
   }
 
@@ -29,33 +29,29 @@ function flipCard(level) {
     cards[i].addEventListener('click', rotationCart);
   }
 }
-
+/* Функция выполняет непосредственно переворот карты и 
+навешивает событие по рестарту для каждой карты*/
 function rotationCart() {
   console.log("функция переворота");
   this.classList.add('rotation');
   
+  //удаляем у всех карт событие переворота
   Array.from(cards).forEach(card => card.removeEventListener('click', rotationCart));
-  this.classList.remove('rotation');
+  //добавляем всем картам событие рестарта 
   Array.from(cards).forEach(card => card.addEventListener('click', restartCartTable));
-
-  /*for (let i=1; i<=level; i++) {
-    const idElem = 'inner' + i;
-    const cart = document.getElementById(idElem);
-    cart.addEventListener('click',  restartCartTable(level));
-  }*/
 }
 
+/* Функция скрывает стол с картами, при этом удаляя все карты
+ и активирует главное меню */
 function restartCartTable() {
   console.log("сюда зашли");
+  
   //главное окно сделали активным, карточный стол скрыли
   content.classList.remove('deactivate');
   cardTable.classList.add('deactivate');
 
-  //удалили все карты
-  cards = document.getElementsByClassName('card');
-  for (let i=1; i<cards.length; i++) {
-    cards[i].remove();
-  }
+  //удалили все карты-копии 
+  document.querySelectorAll('.copy').forEach(elem => elem.remove());
 }
 
 //создание карточного стола
@@ -66,13 +62,8 @@ function openCardTable() {
 
   content.classList.add('deactivate');
   cardTable.classList.remove('deactivate');
+  
   setCards(level);
-
-  /*cards = document.querySelectorAll('.card');
-  console.log(cards);
-  Array.from(cards).forEach(card => card.addEventListener('click', rotationCart()));
-  console.log(cards);*/
-
   flipCard(level);
 }
 
